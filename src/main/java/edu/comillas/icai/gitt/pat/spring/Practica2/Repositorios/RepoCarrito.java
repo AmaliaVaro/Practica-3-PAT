3f6b7e920b43d98ccb85ffca7b3b27bd4ceee3c6
Amalia Varo <amivaro1605@gmail.com>
2026-03-04 23:33:08 +0100

    Repositorios creados

---
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/Practica2/Repositorios/RepoCarrito.java
@@ -0,0 +1,7 @@
+package edu.comillas.icai.gitt.pat.spring.Practica2.Repositorios;
+
+import edu.comillas.icai.gitt.pat.spring.Practica2.Entidades.Carrito;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RepoCarrito extends CrudRepository<Carrito, Long> {
+}
---
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/Practica2/Repositorios/RepoLineaCarrito.java
@@ -0,0 +1,7 @@
+package edu.comillas.icai.gitt.pat.spring.Practica2.Repositorios;
+
+import edu.comillas.icai.gitt.pat.spring.Practica2.Entidades.LineadeCarrito;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RepoLineaCarrito extends CrudRepository<LineadeCarrito, Long> {
+}
